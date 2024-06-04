92add417552a7b214876c8d5fafd2a115dd795e0
Hasan Zonouzi <hasanzon@yahoo.com>
2024-06-05 00:00:27 +0330

    feat: admin files added

--- server/competition/admin.py
+++ server/competition/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Team, Group, ServerConfig, Match, BinaryUpload
 
 # Register your models here.
+admin.site.register(Team)
+admin.site.register(Group)
+admin.site.register(ServerConfig)
+admin.site.register(Match)
+admin.site.register(BinaryUpload)
--- server/runner/admin.py
+++ server/runner/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Runner, RunnerServer
+
 # Register your models here.
+admin.site.register(Runner)
+admin.site.register(RunnerServer)
