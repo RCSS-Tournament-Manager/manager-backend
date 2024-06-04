dd8a47c27ad632711e2040fc7026b6bdc2bd4c77
Hasan Zonouzi <hasanzon@yahoo.com>
2024-06-04 15:04:19 +0330

    feat: initiate user app and user model

---
+++ server/user/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django_minio_backend import MinioBackend, iso_date_prefix
+
+from server.enums import Roles
+
+
+# Create your models here.
+class User(AbstractUser):
+    role = models.PositiveSmallIntegerField(choices=Roles.choices)
+    team = models.ForeignKey('competition.Team', on_delete=models.PROTECT, related_name="members")
+    avatar = models.FileField(verbose_name="avatar-upload",
+                              upload_to=iso_date_prefix,
+                              storage=MinioBackend(bucket_name="user-avatar-private"))
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
