6ec6609dd9bef67f7ec777fe76c433882ba5792e
Hasan Zonouzi <hasanzon@yahoo.com>
2024-06-05 00:07:39 +0330

    fix: rest framework added to installed apps

--- server/server/settings.py
+++ server/server/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.postgres",
+    "rest_framework",
+    "rest_framework.authtoken",
     "user",
     "competition",
     "news",
