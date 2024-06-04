3698a5ab1c6efef624123c4119e7a02bc61a380f
Hasan Zonouzi <hasanzon@yahoo.com>
2024-06-05 00:01:06 +0330

    fix: setting auth user added

--- server/server/settings.py
+++ server/server/settings.py
@@ -147,7 +147,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-# AUTH_USER_MODEL = 'user.User'
+AUTH_USER_MODEL = "user.User"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
