f928032d94489df0eaf2c12b8c42aa4acacf1b68
Hasan Zonouzi <hasanzon@yahoo.com>
2024-06-10 17:56:07 +0330

    fix: team name shown in user api

--- server/user/serializers.py
+++ server/user/serializers.py
@@ -4,6 +4,8 @@ from user.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    team = serializers.SlugRelatedField(read_only=True, slug_field="name")
+
     class Meta:
         model = User
         fields = ("id", "username", "email", "first_name", "last_name", "role", "team")
